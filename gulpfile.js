0ecbe03a74f3785c5f1819d79c82f427c4aa2b7c
Francisco Mazzoni <fran.mazzoni@gmail.com>
2016-03-02 15:51:05 -0300

    Added images with 2 sizes for srcset, gulpfile for batch resizing and css changes to the site

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
 var gulp = require('gulp');
+var imageResize = require('gulp-image-resize');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
   // place code for your default task here
-  console.log("hey")
+
+  gulp.src("raw_images/*")
+    .pipe(imageResize({
+      width: 2048,
+      quality: 0.5,
+      imageMagick: true
+    }))
+    .pipe(rename(function (path){
+      path.basename += "-2x";
+    }))
+    .pipe(gulp.dest('images'));
+
+    gulp.src("raw_images/*")
+      .pipe(imageResize({
+        width: 1024,
+        quality: 0.5,
+        imageMagick: true
+      }))
+      .pipe(rename(function (path){
+        path.basename += "-1x";
+      }))
+      .pipe(gulp.dest('images'));
 
 });
