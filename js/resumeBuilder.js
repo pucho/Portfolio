da2595b13c66a4377980dc5d200af405cd02486b
Francisco Mazzoni <fran.mazzoni@gmail.com>
2016-03-06 18:11:13 -0300

    Fixed functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -86,63 +86,69 @@ var education = {
   }]
 }
 
-bio.display = function() {
-	$("#bio").append("<h2>Contact</h2>")
-	for (index in bio.contacts){
-		var bioDataTemplate = `<div class="col-md-${12 / Object.keys(bio.contacts).length}">
-			<h3>${index}</h3><h5>${bio.contacts[index]}</h5>`
-		$("#bio").append(bioDataTemplate)
-	}
-	$("#bio").append("<h2>Skills</h2>")
-	for(index in bio.skills){
-		var skillsTemplate = `<div class="col-md-${12 / Object.keys(bio.skills).length}">
-			<h5>${bio.skills[index]}</h5>`
-		$("#bio").append(skillsTemplate)
-	}
-	$("#bio").append("</div")
-}
-
-work.display = function() {
-	$("#work").append("<hr><h2>Work Experience</h2><hr>")
-	for (job in work.jobs){
-		var jobDataTemplate = `<h3>${work.jobs[job].title} at ${work.jobs[job].employer} in ${work.jobs[job].location} (${work.jobs[job].dates})</h3>
-													 <h4>${work.jobs[job].description}</h4>`
-		$("#work").append(jobDataTemplate)
-	}
-}
-
-projects.display = function() {
-	$("#projects").append("<hr><h2>Projects</h2><hr>")
-	for (index in projects.projects){
-		var images = ""
-		for (image in projects.projects[index].images) {
-			images += `<img class'img-responsive' src='${projects.projects[index].images[image]}'>`
+bio.display = $( document ).ready(
+	function() {
+		$("#bio").append("<h2>Contact</h2>")
+		for (index in bio.contacts){
+			var bioDataTemplate = `<div class="col-md-${12 / Object.keys(bio.contacts).length}">
+				<h3>${index}</h3><h5>${bio.contacts[index]}</h5>`
+			$("#bio").append(bioDataTemplate)
 		}
-		console.log(images)
-		var projectTemplate = `<h3>${projects.projects[index].title} in ${projects.projects[index].dates}</h3>
-													 <h4>${projects.projects[index].description}</h4>
-													 ${images}
-													 `
-		console.log(projectTemplate)
-		$("#projects").append(projectTemplate)
+		$("#bio").append("<h2>Skills</h2>")
+		for(index in bio.skills){
+			var skillsTemplate = `<div class="col-md-${12 / Object.keys(bio.skills).length}">
+				<h5>${bio.skills[index]}</h5>`
+			$("#bio").append(skillsTemplate)
+		}
+		$("#bio").append("</div")
 	}
-}
+)
 
-education.display = function () {
-	$("#education").prepend("<hr><h2>Formal education</h2><hr>")
-	for (index in education.schools){
-		var school = education.schools[index]
-		var educationTemplate = `<h3><a href="${school.url}"></a>${school.name} located in ${school.location}</h3>
-														 <h4>Graduated in ${school.dates} with a degree of ${school.degree} majoring in ${school.majors}</h4>
-														 `
-	 $("#education").append(educationTemplate)
+work.display = $( document ).ready(
+	function() {
+		$("#work").append("<hr><h2>Work Experience</h2><hr>")
+		for (job in work.jobs){
+			var jobDataTemplate = `<h3>${work.jobs[job].title} at ${work.jobs[job].employer} in ${work.jobs[job].location} (${work.jobs[job].dates})</h3>
+													 	<h4>${work.jobs[job].description}</h4>`
+			$("#work").append(jobDataTemplate)
+		}
 	}
-	$("#education").append("<hr><h2>Online education</h2><hr>")
-	for (index in education.onlineCourses){
-		var school = education.onlineCourses[index]
-		var educationTemplate = `<h3><a href="${school.url}"></a>${school.school}</h3>
-														 <h4>Graduated in ${school.dates} with a title of ${school.title}</h4>
-														 `
-	 $("#education").append(educationTemplate)
+)
+
+projects.display = $( document ).ready(
+	function() {
+		$("#projects").append("<hr><h2>Projects</h2><hr>")
+		for (index in projects.projects){
+			var images = ""
+			for (image in projects.projects[index].images) {
+				images += `<img class'img-responsive' src='${projects.projects[index].images[image]}'>`
+			}
+			var projectTemplate = `<h3>${projects.projects[index].title} in ${projects.projects[index].dates}</h3>
+													 	<h4>${projects.projects[index].description}</h4>
+													 	${images}
+													 	`
+			$("#projects").append(projectTemplate)
+		}
 	}
-}
+)
+
+education.display = $( document ).ready(
+	function () {
+		$("#education").prepend("<hr><h2>Formal education</h2><hr>")
+		for (index in education.schools){
+			var school = education.schools[index]
+			var educationTemplate = `<h3><a href="${school.url}"></a>${school.name} located in ${school.location}</h3>
+															 <h4>Graduated in ${school.dates} with a degree of ${school.degree} majoring in ${school.majors}</h4>
+															 `
+		 $("#education").append(educationTemplate)
+		}
+		$("#education").append("<hr><h2>Online education</h2><hr>")
+		for (index in education.onlineCourses){
+			var school = education.onlineCourses[index]
+			var educationTemplate = `<h3><a href="${school.url}"></a>${school.school}</h3>
+															 <h4>Graduated in ${school.dates} with a title of ${school.title}</h4>
+															 `
+		 $("#education").append(educationTemplate)
+		}
+	}
+)
